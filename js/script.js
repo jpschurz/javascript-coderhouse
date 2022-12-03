30ffd3c5860f15a3499de30bb8337caed8d39c23
Juan Pablo <juanpabloschurz@gmail.com>
2022-12-03 15:34:07 -0300

    Correción de codigo y diseño

--- js/script.js
+++ js/script.js
@@ -1,100 +1,108 @@
 //Elementos DOM
-const btn = document.querySelector('#btn')
-const foto = document.querySelector('.foto')
+const btn = document.querySelector('#btn');
 const lista = document.querySelector('#productos');
 const btnProductos = document.querySelector('#btnProductos');
 const btnCarrito = document.querySelector('#btnCarrito');
 let carritoHTML = document.querySelector('#prodCarrito');
 
-//Clase constructora de objetos
-/*class Producto {
-    constructor(categoriaProducto, nombreProducto, bandaProducto, precioProducto, stockProducto) {
-        this.categoria = categoriaProducto;
-        this.banda = bandaProducto;
-        this.nombre = nombreProducto;
-        this.precio = precioProducto;
-        this.stock = parseInt(stockProducto)
-    }
-
-}*/
 
 //Lista de productos
-/*const producto_0 = new producto('CD', 'Slayer', 'Repentless', 2100, 15);
-const producto_1 = new producto('DVD', 'Judas Priest', 'Live in London', 3500, 4);
-const producto_2 = new producto('VINILO', 'Judas Priest', 'Killing Machine', 2200, 2);
-const producto_3 = new producto('BLUERRAY', 'Metallica', 'The Big 4 Live from Sofia, Bulgaria', 4200, 8);
-
-const listaProductos = [producto_0, producto_1, producto_2, producto_3];*/
-
-
 const DB = [{
     id: 1,
     nombre: 'Repentless',
+    categoria: 'CD',
     precio: 2100,
-    img: "<img src='images/productos/cd/judas-priest-painkiller.jpg' />"
+    img: "<img class='img-conciertos' src='images/productos/cd/judas-priest-painkiller.jpg' />"
     
 },
 {
     id: 2,
     nombre: 'Live in London',
+    categoria: 'DVD',
     precio: 3500,
-    img: "<img src='images/productos/dvd/judas-priest-live-in-london.jpeg' />"
+    img: "<img class='img-conciertos' src='images/productos/dvd/judas-priest-live-in-london.jpeg' />"
 },
 {
     id: 3,
     nombre: 'Killing Machine',
+    categoria: 'VINILO',
     precio: 2200,
-    img: "<img src='images/productos/vinilos/judas-priest-killing-machine.jpg' />"
+    img: "<img class='img-conciertos' src='images/productos/vinilos/judas-priest-killing-machine.jpg' />"
 },
 {
     id: 4,
     nombre: 'The Big 4 Live from Sofia, Bulgaria',
+    categoria: 'BLUERRAY',
     precio: 4200,
-    img: "<img src='images/productos/bluerray/metallica-the-big-4-live-from-sofia-bulgaria.jpg' />"
+    img: "<img class='img-conciertos' src='images/productos/bluerray/metallica-the-big-4-live-from-sofia-bulgaria.jpg' />"
 },
 ]
 
+
+//Solicitar productos
 const pedirProductos = ()=>{
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
             resolve(DB)
-        },2000)
+        },100)
     })
 }
-
 
 
 let carrito = []
 
 
-let tiempo = 2500;
-btn.onclick=()=>{
-    foto.classList.add('visible');
-    setTimeout(()=>{
-        foto.classList.remove('visible')
-    },tiempo);
-}
-
+//Muestra productos
 const renderProductos =(array)=>{
 array.forEach(element=>{
-    let html = `<li>${element.nombre} $ ${element.precio}</li>
-    ${element.img}    
-    <button class="agregar" id=${element.nombre}>Agregar</button>`
+    let html = `    
+        <section>        
+            <div class="grid-img-conciertos">
+            ${element.img}
+                </div>
+                <br>
+                <p>${element.nombre} <br> ${element.categoria}</p>
+                <p>$ ${element.precio}</p>
+                <button class="agregar btn btn-light btn-sm" id=${element.nombre}>
+                Comprar
+                </button>
+                </div>
+            </div>
+        </section>
+    `
     lista.innerHTML += html;
 })
 }
 
+//Compra producto
+btnProductos.addEventListener('click',()=>{ 
+    
+    //Filtro categoria
+    /*const seleccionarCategoria = document.getElementById('btnProductos');
 
-btnProductos.addEventListener('click',()=>{
+    function selectCategoria(DB, categoria){
+        return DB.filter(categoria => categoria.tipo == tipo);
+    };
+
+    function mostrarCategoria(DB) {
+        for (const categoria of DB) {
+            let option = `${element.categoria}`
+
+            categoria.innerHTML += option;
+        }
+    }
+    console.log(selectCategoria)*/
+
     pedirProductos().then((response)=>{
         let productos = response;
-        console.log(productos);
+        //console.log(productos);
         renderProductos(productos);
     }).then(()=>{
         const btnAgregar = document.querySelectorAll('button.agregar');
             //console.log(btnAgregar);
             btnAgregar.forEach(element => {
                 element.onclick = () => {
+                    alertaProductoAgregado()
                     carrito.push(element.id)
                 }
             })
@@ -102,13 +110,32 @@ btnProductos.addEventListener('click',()=>{
 })
 
 
-btnCarrito.onclick = () => {
-    //carritoHTML.innerHTML = '';
-    carrito.forEach(element => {
-        let html = `<li>${element} agregado al carrito</li>`
-        carritoHTML.innerHTML += html
-
-    });
+//Alerta de producto agregado
+function alertaProductoAgregado(){
+    Toastify({
+        text: 'Producto agregado al carrito',
+        duration: 2000,
+        style: {
+            color: 'white',
+            width: '20vw',
+            height: 80,
+            background: 'rgba(139,0,0,1)',
+        },
+        offset: {
+            x: 50,
+            y: 10,
+        },
+    }).showToast();
 }
 
 
+//Muestra carrito
+btnCarrito.onclick = () => {
+    carritoHTML.innerHTML = '';
+    carrito.forEach(element => {
+        let html = `
+        <li>${element}</li>        
+        `
+        carritoHTML.innerHTML += html
+    });
+}
